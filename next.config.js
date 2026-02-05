4277b6b43abfe4cf50610a6b2dd77f9c8feeab7e
ŞENAY AKAGÜNDÜZ <senayakagunduz@192.168.1.105>
2026-02-05 09:31:21 +0300

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,24 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        // Apply these headers to all API routes
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
-      },
-    ];
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export const middleware = withAuth(
+  function middleware(request) {
+    return NextResponse.next();
   },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  }
+);
+
+export const config = {
+  matcher: ["/dashboard/:path*", "/profile/:path*"],
 };
-
-module.exports = nextConfig;
-
